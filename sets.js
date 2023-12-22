d2e135b1c48667606d2cb04f98603d2a4a4309b2
hazem radwan <hazem.hemily@gmail.com>
2023-12-23 00:15:11 +0200

    add sets.js file

---
+++ sets.js
@@ -0,0 +1,6 @@
+const set  = new Set([1,2,3]) ;  // it's just an array with unique values only
+set.add(4) ; // add to  set 
+set.delete(4) ; // delete from a set 
+set.has(4) //  check if value is there or not
+set.size // return set's length 
+set.clear() // empty the set from its values
