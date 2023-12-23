59625b094d9e53e112b23c2d4c8ee6559905d436
hazem radwan <hazem.hemily@gmail.com>
2023-12-23 16:09:51 +0200

    add more logic to stack.js file

--- stack.js
+++ stack.js
@@ -16,13 +16,13 @@ class Stack {
   }
 
   getFront() {
-    if (!this.isEmpty) {
+    if (!this.isEmpty()) {
       return this.stack[this.getSize() - 1]
     }
     console.log('stack is empty')
   }
   getRear() {
-    if (!this.isEmpty) {
+    if (!this.isEmpty()) {
       return this.stack[0]
     }
     console.log('stack is empty')
@@ -32,3 +32,19 @@ class Stack {
     return this.stack.length === 0
   }
 }
+
+const stack = new Stack()
+stack.push(10)
+stack.push(20)
+stack.push(30)
+stack.push(40)
+stack.getSize()
+console.log(stack.getRear(), ' => Rear')
+console.log(stack.getFront(), ' => Front ')
+console.log(stack.pop(), 'is out of the stack now')
+console.log(stack.stack.toString())
+console.log('is the stack empty ?', stack.isEmpty())
+console.log(stack.pop(), 'is out of the stack now')
+console.log(stack.pop(), 'is out of the stack now')
+console.log(stack.pop(), 'is out of the stack now')
+console.log('is the stack empty ?', stack.isEmpty())
