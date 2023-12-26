aa7767a7c8b3be3c62e2419016ed3d0f189f526f
hazem radwan <hazem.hemily@gmail.com>
2023-12-26 11:45:34 +0200

    add prepend(value) and print() function to linked list

--- linkedList.js
+++ linkedList.js
@@ -20,21 +20,37 @@ class LinkedList {
 
   prepend(value) {
     const node = new Node(value)
-    this.head = node
     if (!this.isEmpty()) {
       node.next = this.head
     }
+    this.head = node
     this.size++
   }
+  print() {
+    if (this.isEmpty()) {
+      console.log('this linked list is empty')
+      return
+    }
+    let values = []
+    let current = this.head
+    while (current !== null) {
+      values.push(current.value)
+      current = current.next
+    }
+    console.log(values)
+  }
+
 }
 
 const linkedList = new LinkedList()
 linkedList.prepend(10)
 linkedList.prepend(20)
 linkedList.prepend(30)
+linkedList.append(40)
 console.log(
   `this linked list  has ${linkedList.getSize()} ${
     linkedList.getSize() === 1 ? 'node' : 'nodes'
   }`,
 )
 console.log(`Head has the value of ${linkedList.head?.value} now`)
+linkedList.print()
