b503639ffa191ba8c7f006d4c9e8cbcd82f69537
hazem radwan <hazem.hemily@gmail.com>
2023-12-27 14:39:54 +0200

    add searchByValue(value) to linked list file

--- linkedList.js
+++ linkedList.js
@@ -1,124 +1,137 @@
 class Node {
   constructor(value) {
-    this.value = value
-    this.next = null
+    this.value = value;
+    this.next = null;
   }
 }
 
 class LinkedList {
   constructor() {
-    this.head = null
-    this.size = 0
+    this.head = null;
+    this.size = 0;
   }
 
   isEmpty() {
-    return this.size === 0
+    return this.size === 0;
   }
   getSize() {
-    return this.size
+    return this.size;
   }
 
   prepend(value) {
-    const node = new Node(value)
+    const node = new Node(value);
     if (!this.isEmpty()) {
-      node.next = this.head
+      node.next = this.head;
     }
-    this.head = node
-    this.size++
+    this.head = node;
+    this.size++;
   }
 
   append(value) {
-    const node = new Node(value)
+    const node = new Node(value);
     if (this.isEmpty()) {
-      this.head = node
+      this.head = node;
     } else {
-      let current = this.head
+      let current = this.head;
       while (current.next !== null) {
-        current = current.next
+        current = current.next;
       }
-      current.next = node
+      current.next = node;
     }
-    this.size++
+    this.size++;
   }
 
   insert(value, index) {
-    if (index < 0 || index > this.size - 1) return
+    if (index < 0 || index > this.size - 1) return;
     if (index === 0) {
-      this.prepend(value)
-      return
+      this.prepend(value);
+      return;
     }
-    const node = new Node(value)
-    let currentIndex = 0
-    let currentNode = this.head
+    const node = new Node(value);
+    let currentIndex = 0;
+    let currentNode = this.head;
     while (currentIndex < index - 1) {
-      currentNode = currentNode.next
-      currentIndex++
+      currentNode = currentNode.next;
+      currentIndex++;
     }
-    node.next = currentNode.next
-    currentNode.next = node
-    this.size++
+    node.next = currentNode.next;
+    currentNode.next = node;
+    this.size++;
   }
 
   removeByIndex(index) {
-    if (index < 0 || this.size <= index) return null
-    let node = this.head
+    if (index < 0 || this.size <= index) return null;
+    let node = this.head;
     if (index === 0) {
-      this.head = this.head.next
+      this.head = this.head.next;
     } else {
       for (let i = 0; i < index - 1; i++) {
-        node = node.next
+        node = node.next;
       }
-      node.next = node.next.next
+      node.next = node.next.next;
     }
-    this.size--
-    return node.value
+    this.size--;
+    return node.value;
   }
   removeByValue(value) {
-    if (this.isEmpty()) return null
-    let curr = this.head
-    let prev = null
+    if (this.isEmpty()) return null;
+    let curr = this.head;
+    let prev = null;
     if (this.head.value === value) {
-      this.head = this.head.next
-      this.size--
-      return curr.value
+      this.head = this.head.next;
+      this.size--;
+      return curr.value;
     }
     for (let i = 0; i < this.getSize(); i++) {
       if (curr.value !== value) {
-        prev = curr
-        curr = curr.next
+        prev = curr;
+        curr = curr.next;
       } else {
-        prev.next = curr.next
-        this.size--
-        return curr.value
+        prev.next = curr.next;
+        this.size--;
+        return curr.value;
       }
     }
-    return null
+    return null;
+  }
+
+  searchByValue(value) {
+    let index = 0;
+    let temp = this.head;
+    while (temp !== null) {
+      if (temp.value === value) {
+        return index;
+      }
+      temp = temp.next;
+      index++;
+    }
+    return -1;
   }
 
   print() {
     if (this.isEmpty()) {
-      console.log('this linked list is empty')
-      return
+      console.log("this linked list is empty");
+      return;
     }
-    let values = []
-    let current = this.head
+    let values = [];
+    let current = this.head;
     while (current !== null) {
-      values.push(current.value)
-      current = current.next
+      values.push(current.value);
+      current = current.next;
     }
-    console.log(values)
+    console.log(values);
   }
 }
 
-const linkedList = new LinkedList()
-linkedList.prepend(10)
-linkedList.prepend(20)
-linkedList.prepend(30)
-linkedList.append(40)
-linkedList.insert(50, 1)
-
-linkedList.removeByIndex(0)
-linkedList.removeByIndex(2)
+const linkedList = new LinkedList();
+linkedList.prepend(10);
+linkedList.prepend(20);
+linkedList.prepend(30);
+linkedList.append(40);
+linkedList.insert(50, 1);
+console.log(linkedList.searchByValue(40));
+// linkedList.removeByIndex(0);
+// linkedList.removeByIndex(2);
 
 // linkedList.removeByValue(40)
 // linkedList.removeByValue(10)
@@ -126,8 +139,8 @@ linkedList.removeByIndex(2)
 
 console.log(
   `this linked list  has ${linkedList.getSize()} ${
-    linkedList.getSize() === 1 ? 'node' : 'nodes'
-  }`,
-)
-console.log(`Head has the value of ${linkedList.head?.value} now`)
-linkedList.print()
+    linkedList.getSize() === 1 ? "node" : "nodes"
+  }`
+);
+console.log(`Head has the value of ${linkedList.head?.value} now`);
+linkedList.print();
