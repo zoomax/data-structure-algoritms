b87b5b72f7c6fb7de8c23102da8284e5b3c113da
hazem radwan <hazem.hemily@gmail.com>
2024-01-08 09:51:37 +0200

    add insert(value) functionality to binary search tree

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,4 +1,4 @@
-class node {
+class Node {
   constructor(value) {
     this.value = value;
     this.left = null;
@@ -10,7 +10,40 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
-  isEmpty() { 
-    return this.root === null ; 
+  isEmpty() {
+    return this.root === null;
+  }
+
+  insert(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.root = node;
+      console.log(node.value, " => node inserted on in the root node");
+
+      return;
+    }
+    this.insertNode(node, this.root);
+  }
+  insertNode(node, root) {
+    if (node.value > root.value) {
+      if (root.right === null) {
+        root.right = node;
+        console.log(node.value, " => node inserted on in the right side");
+      } else {
+        this.insertNode(node, root.right);
+      }
+    }
+    if (node.value < root.value) {
+      if (root.left === null) {
+        root.left = node;
+        console.log(node.value, " => node inserted on in the left side");
+      } else {
+        this.insertNode(node, root.left);
+      }
+    }
   }
 }
+
+const bst = new BinarySearchTree();
+bst.insert(100);
+bst.insert(200), bst.insert(50);
